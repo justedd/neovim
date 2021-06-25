0fbac845d3620c0c5d56d8ceea63d040a18710bc
justed <dev.justed@gmail.com>
2021-06-26 00:24:36 +0500

    stupid icons

--- lua/justed.lua
+++ lua/justed.lua
@@ -14,36 +14,25 @@ require'lspconfig'.solargraph.setup{
   }
 }
 
---require "pears".setup()
-require('nvim-autopairs').setup()
-
+require'lspconfig'.rust_analyzer.setup{
+  cmd = { "/home/justed/core/apps/rust-analyzer" },
+  --settings = {
+    --["rust-analyzer"] = {
+      --["diagnostics.enable"] = false
+    --}
+  --}
+  handlers = {
+    ["textDocument/publishDiagnostics"] = vim.lsp.with(
+      vim.lsp.diagnostic.on_publish_diagnostics, {
+        -- Disable virtual_text
+        virtual_text = false,
+        signs = false
+      }
+    )
+  },
+}
 
 saga.init_lsp_saga({})
-
-
-
-local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
-
--- skip it, if you use another global object
-_G.MUtils= {}
-
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm=function()
-  if vim.fn.pumvisible() ~= 0  then
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-    else
-      return npairs.esc("<cr>")
-    end
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
-
-remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
-
 
 require('telescope').setup {
   defaults = {
