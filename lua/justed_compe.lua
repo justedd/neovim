2ad6cb4983f61abde40c8e478590da1b07fc33fc
justed <dev.justed@gmail.com>
2021-08-01 15:58:58 +0500

    painful complete

--- lua/justed_compe.lua
+++ lua/justed_compe.lua
@@ -8,43 +8,60 @@ end
 function Source.get_metadata(self)
   return {
     priority = 10;
-    menu = '[Shared Examples]';
+    menu = '[RSpec]';
     filetypes = {"ruby"}
   }
 end
-
-
-local cmd=[=[
-  !ack shared_examples ./spec/support --no-color --no-group | sed "s/^[^']*'\([^']*\)'.*$/\1/" | sed "/:/d" | sort -u
-]=]
 
 function Source.determine(self, context)
   return compe.helper.determine(context)
 end
 
-local result = vim.api.nvim_exec(cmd, true)
+local SharedExamples = {
+  loaded = false;
+}
 
-lines = {}
-for line in result:gmatch("[^\r\n]+") do
-  table.insert(lines, line)
-end
+function SharedExamples:get()
+  if self.loaded then
+    return self.items
+  else
+    local cmd=[=[
+      !ack shared_examples ./spec/support --no-color --no-group | sed "s/^[^']*'\([^']*\)'.*$/\1/" | sed "/:/d" | sort -u
+    ]=]
 
-table.remove(lines,1)
+    local result = vim.api.nvim_exec(cmd, true)
 
-autocomplete_items = {}
-for key, line in pairs(lines) do
-  table.insert(
-    autocomplete_items,
-    { word = "it_behaves_like '" .. line .. "'" }
-  )
+    local lines = {}
+    for line in result:gmatch("[^\r\n]+") do
+      table.insert(lines, line)
+    end
+
+    table.remove(lines,1)
+
+
+    local items = {}
+    for key, line in pairs(lines) do
+      table.insert(
+        items,
+        { word = line }
+      )
+    end
+
+    self.items = items
+    self.loaded = true
+
+    return items
+  end
 end
 
 function Source.complete(self, context)
-  items = {}
+  local items = {}
   local filename = vim.api.nvim_exec("echo expand('%:t')", true)
+  local line = vim.api.nvim_get_current_line()
 
-  if string.find(filename, "_spec.rb") then
-    items = autocomplete_items
+  if string.find(filename, "_spec.rb") and string.find(line, "it_behaves_like '") then
+    items = SharedExamples:get()
+    print('printed')
   end
 
   context.callback({
